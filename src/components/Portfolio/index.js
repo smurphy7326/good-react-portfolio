277621941dcbff780f41669b95786c43df70bd83
smurphy7326 <smurphy7326@gmail.com>
2021-12-11 10:43:29 -0500

    working on the Portfolio index.js

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-// have to link to the projects that will be put there
+import React, { useState } from 'react';
+import Project from '../Project';
 
 const Portfolio = () => {
+    const [projectsList, setProjectsList] = useState(projects);
     return (
-        <div>
-            Portfolio
+        <div className='row'>
+            <h2 className='header'>Portfolio</h2>
+            {projectsList.map(project => (
+                <Project
+                id={project.id}
+                name={project.name}
+                image={project.image}
+                github={project.github}
+                deploy={project.deploy}
+                description={project.description}
+                key={project.id}
+                />
+            ))}
         </div>
     )
 }
---
+++ src/components/Project/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+// have to link to the projects that will be put there
+
+const Projects = () => {
+    return (
+        <div>
+            Projects
+        </div>
+    )
+}
+
+export default Projects
