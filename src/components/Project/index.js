43729c64ed3d8bea31993eeb6df8f0d43b12f5a5
smurphy7326 <smurphy7326@gmail.com>
2021-12-11 15:08:11 -0500

    working around with portfolio to make it work

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,27 +1,26 @@
 import React from 'react'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // https://materializecss.com/cards.html is where i am getting most of the things for this part. 
-// different links for the github and deployed icons, looking for the different photos in regards to the projects
+
 const Project = (props) => {
     return (
-        <div className='col s12 m6'>
-            <div className='light-blue lighten-1'>
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img className='activator responsive-img' alt={props.name} src={require(`../../assets/images/${props.image}`).default}/>
-                </div>
-                <div className="card-content">
+        <div className='col s12 m4 l4'>
+        <div className="card hoverable grey darken-3">
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator responsive-img" alt={props.name} src={require(`../../assets/images/${props.image}`).default}/>
+            </div>
+            <div className="card-content">
                 <span className="card-title activator">{props.name}<i className="material-icons right">more_vert</i></span>
                 <p>
-                    <a href={props.github}></a>
-                    <a href={props.deploy}></a>
+                    <a href={props.github} target='_blank' rel="noreferrer"></a>
+                    <a href={props.deploy} target='_blank' rel="noreferrer"></a>
                 </p>
-                </div>
-                <div className='card-reveal'>
-                    <span class="card-title grey-text text-darken-4">{props.name}<i class="material-icons right">close</i></span>
-                    <p className='grey-text text-darken-3'> {props.description}</p>
-                </div>
             </div>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-3">{props.name}<i className="material-icons right">close</i></span>
+                <p className='grey-text text-darken-3'>{props.description}</p>
+            </div>
+        </div>
         </div>
     )
 }
