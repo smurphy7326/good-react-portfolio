4300e811f524dccf5419aa753f9e59c31abc6e9d
smurphy7326 <smurphy7326@gmail.com>
2021-12-12 10:51:46 -0500

    footer working have to style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,19 @@
-// import React from 'react'
+import React from 'react'
 
-// const Footer = () => {
-//     return (
-//         <footer style = {{backgroundColor: "#071425"}} className="page-footer">
-//             <div className="container">
-//                 <div className="row">
-//                     <div className="col s12 m6">
-//                         <h4 className="white-text">Footer Content</h4>
-//                         <p className="grey-text text-darken-3"></p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </footer>
-//     )
-// }
+const Footer = () => {
+    return (
+        <footer style = {{backgroundColor: "#1f5aa5"}} className="page-footer">
+            <div className="container center">
+                <div className="row">
+                    <div className="col s12">
+                        <p className="grey-text text-darken-3"></p>
+                        <a href='https://github.com/smurphy7326/good-react-portfolio' target="_blank" rel="noreferrer"i class="fab fa-github-square fa-3x"></a>
+                        <a href='https://www.linkedin.com/in/sean-murphy-08a45a37/' target="_blank" rel="noreferrer" i class="fas fa-paper-plane fa-3x"></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
