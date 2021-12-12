9a1d881b76eb958459384e8596d5a74c4b7cb3b8
smurphy7326 <smurphy7326@gmail.com>
2021-12-11 21:13:43 -0500

    commented out Footer to make sure that it will still run

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+// const Footer = () => {
+//     return (
+//         <footer style = {{backgroundColor: "#071425"}} className="page-footer">
+//             <div className="container">
+//                 <div className="row">
+//                     <div className="col s12 m6">
+//                         <h4 className="white-text">Footer Content</h4>
+//                         <p className="grey-text text-darken-3"></p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </footer>
+//     )
+// }
