cf97940f9e4eda3f40f5c75a4862ca105d7182a2
smurphy7326 <smurphy7326@gmail.com>
2021-12-11 20:31:32 -0500

    still working on resume

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,11 +10,11 @@ const {
 
     return (
         <Col id ="navigationBar">
-            <Navbar collapseOnSelect expand='lg'>
+            <Navbar style={{ backgroundColor: "#071425" }} collapseOnSelect expand="lg" variant="dark">
                 <Navbar.Brand id='nav-brand-id' eventKey="about" onClick={() => handlePageChange('about')}>Sean Murphy</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" onSelect= {(selectedKey) => handlePageChange(selectedKey)}>
+                    <Nav className="justify-content-end" style={{ width: "100%" }} onSelect= {(selectedKey) => handlePageChange(selectedKey)}>
                     <Nav.Link eventKey="about"><span className={currentPage==='about' ? 'activeSection' : 'inactiveSection'}>About Me</span></Nav.Link>
                     <Nav.Link eventKey="portfolio"><span data-testid="portfolio-nav-item" className={currentPage==='portfolio' ? 'activeSection' : 'inactiveSection'}>Portfolio</span></Nav.Link>
                     <Nav.Link eventKey="contact"><span className={currentPage==='contact' ? 'activeSection' : 'inactiveSection'}>Contact</span></Nav.Link>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,27 +3,27 @@ import React from 'react'
 const Resume = () => {
     return (
         <div className='row'>
-            <h3 className='header'>Resume <span><img src='/favicon.ico' alt='aboutIcon'></img></span></h3>
+            <h3 className='header'>Resume <span><img src='UCONNlogo.png' alt='uconnLogo'></img></span></h3>
             <div className='row'>
-                <h5 className='col s12'>Click on this link to download my resume! <a href='https://docs.google.com/document/d/18EsiYCBKOuDHvzqX3ybUkaVWs-yp0pxyj428RD4Y9nM/edit?usp=sharing' target='_blank'>Resume</a></h5>
+                <h5 className='col s12'>Click on this link to download my resume! <a href='https://docs.google.com/document/d/18EsiYCBKOuDHvzqX3ybUkaVWs-yp0pxyj428RD4Y9nM/edit?usp=sharing' target='_blank'>Click Here!</a></h5>
             </div>
             <div className='divider'></div>
             <div className='row'>
-                <h4 className='col s12'>Technologies</h4>
+                <h4 className='col s12 center-align'>Technologies</h4>
             </div>
             <div className='row'>
             <div className="col s12 m6">
                 <div className="card blue">
                     <div className="card-content">
-                        <h3 className="card-title">Front-End Technologies</h3>
-                        <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>jQuery</li>
-                        <li>Bootstrap</li>
-                        <li>Materialize</li>
-                        <li>Handlebars.js</li>
-                        <li>React</li>
+                        <h3 className="card-title center-align">Front-End Technologies</h3>
+                        <ul class="collection">
+                        <li class="collection-item center-align card blue">HTML</li>
+                        <li class="collection-item center-align card blue">CSS</li>
+                        <li class="collection-item center-align card blue">jQuery</li>
+                        <li class="collection-item center-align card blue">Bootstrap</li>
+                        <li class="collection-item center-align card blue">Materialize</li>
+                        <li class="collection-item center-align card blue">Handlebars.js</li>
+                        <li class="collection-item center-align card blue">React</li>
                         </ul>
                     </div>
                 </div>
@@ -31,15 +31,15 @@ const Resume = () => {
             <div className="col s12 m6">
                 <div className="card-content">
                     <div className="card blue">
-                    <h3 className="card-title">Back-End Technologies</h3>
-                    <ul>
-                        <li>MySQL</li>
-                        <li>Sequelize</li>
-                        <li>MongoDB</li>
-                        <li>Mongoose</li>
-                        <li>Heroku</li>
-                        <li>Node.js</li>
-                        <li>Express.js</li>
+                    <h3 className="card-title center-align">Back-End Technologies</h3>
+                    <ul class="collection">
+                        <li class="collection-item center-align card blue">MySQL</li>
+                        <li class="collection-item center-align card blue">Sequelize</li>
+                        <li class="collection-item center-align card blue">MongoDB</li>
+                        <li class="collection-item center-align card blue">Mongoose</li>
+                        <li class="collection-item center-align card blue">Heroku</li>
+                        <li class="collection-item center-align card blue">Node.js</li>
+                        <li class="collection-item center-align card blue">Express.js</li>
                     </ul>
                 </div>
             </div>
